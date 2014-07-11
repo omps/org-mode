c9c29e519c24682656662df835f4338e705c56fb
omps.thinkpad.perl <ompnix@gmail.com>
2014-07-11 22:35:20 +0530

    org agenda setup complete

--- org.el
+++ org.el
@@ -132,10 +132,57 @@
 			    (org-agenda-skip-function 'omps/skip-non-stuck-projects)
 			    (org-agenda-sorting-strategy
 			     '(category-keep))))
-		(tags-todo "-HOLD-CANCELLED/"
+		(tags-todo "-HOLD-CANCELLED/!"
 			   ((org-agenda-overriding-header "Projects")
 			    (org-agenda-skip-function 'omps/skip-non-projects)
 			    (org-tags-match-list-sublevels 'indented)
 			    (org-agenda-sorting-strategy
 			     '(category-keep))))
 		(tags-todo "-CANCELLED/!NEXT"
+			   ((org-agenda-overiding-header (concat "Project Next Tasks"
+								 (if omps/hide-scheduled-and-waiting-next-tasks
+								     ""
+								   " (including WAITING and SCHEDULED tasks)")))
+			    (org-agenda-skip-function 'omps/skip-project-and-habits-and-single-tasks)
+			    (org-tags-match-list-sublevels t)
+			    (org-agenda-todo-ignore-scheduled omps/hide-scheduled-and-waiting-next-tasks)
+			    (org-agenda-todo-ignore-deadlines omps/hide-scheduled-and-waiting-next-tasks)
+			    (org-agenda-todo-ignore-with-date omps/hide-scheduled-and-waiting-next-tasks)
+			    (org-agenda-sorting-strategy
+			     '(todo-state-down effort-up category-keep))))
+		(tags-todo "-REFILE-CANCELLED-WAITING-HOLD/!"
+			   ((org-agenda-overriding-header (concat "Project Subtasks"
+								  (if omps/hide-scheduled-and-waiting-next-tasks
+								      ""
+								    " (including WAITING and SCHEDULED tasks)")))
+			    (org-agenda-skip-function 'omps/skip-non-project-tasks)
+			    (org-agenda-todo-ignore-scheduled omps/hide-scheduled-and-waiting-next-tasks)
+			    (org-agenda-todo-ignore-deadlines omps/hide-scheduled-and-waiting-next-tasks)
+			    (org-agenda-todo-ignore-with-date omps/hide-scheduled-and-waiting-next-tasks)
+			    (org-agenda-sorting-strategy
+			     '(category-keep))))
+		(tags-todo "-REFILE-CANCELLED-WAITING-HOLD/!"
+			   ((org-agenda-overriding-header (concat "Standalone Tasks"
+								  (if omps/hide-scheduled-and-waiting-next-tasks
+								      ""
+								    " (including WAITING and SCHEDULED tasks)")))
+			    (org-agenda-skip-function 'omps/skip-project-tasks)
+			    (org-agenda-todo-ignore-scheduled omps/hide-scheduled-and-waiting-next-tasks)
+			    (org-agenda-todo-ignore-deadlines omps/hide-scheduled-and-waiting-next-tasks)
+			    (org-agenda-todo-ignore-with-date omps/hide-scheduled-and-waiting-next-tasks)
+			    (org-agenda-sorting-strategy
+			     '(category-keep))))
+		(tags-todo "-CANCELLED+WAITING|HOLD/!"
+			   ((org-agenda-overriding-header (concat "Waiting and Postpones Tasks"
+								  (if omps/hide-scheduled-and-waiting-next-tasks
+								      ""
+								    " (including WAITING and SCHEDULED tasks)")))
+			    (org-agenda-skip-function 'omps/skip-non-tasks)
+			    (org-tags-match-list-sublevels nil)
+			    (org-agenda-todo-ignore-scheduled omps/hide-scheduled-and-waiting-next-tasks)
+			    (org-agenda-todo-ignore-deadlines omps/hide-scheduled-and-waiting-next-tasks)))
+		(tags "-REFILE/"
+		      ((org-agenda-overriding-header "Tasks to Archive")
+		       (org-agenda-skip-function 'omps/skip-non-archivable-tasks)
+		       (org-tags-match-list-sublevels nil))))
+	       nil))))
